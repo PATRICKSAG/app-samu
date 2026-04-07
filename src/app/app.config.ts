781aee2b5d0c4755dd83bbe041ad49f52a41861d
P4TR1CK SAG <sanandreas86@hotmail.com>
2026-04-07 07:35:02 -0500

    Primer commit: Proyecto Angular con PrimeNG y SCSS

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,19 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes),
+    provideAnimations(),
+    providePrimeNG({
+      theme: {
+        preset: Aura
+      }
+    })
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,20 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';  
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [ButtonModule, CardModule, InputTextModule],  
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('app-samu');
+  title = 'mi-primera-app';
+  
+  // Método de ejemplo para el botón
+  mostrarMensaje() {
+    alert('¡Funciona PrimeNG!');
+  }
 }
